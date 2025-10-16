17631dd838b94348eb4fb203c49148af2f5c23e7
Ivan <ivangonzalogomez@gmail.com>
2025-10-16 16:38:37 -0300

    UI implemented

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import ChatInterface from "./components/ChatInterface"
+
+function App() {
+  return (
+    <div className="min-h-screen bg-background">
+      <ChatInterface />
+    </div>
+  )
+}
+
+export default App
